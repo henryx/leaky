e7b76fd6f07861a66fd6ec09a6f9fb07d4a2741b
Enrico Bianchi <enrico.bianchi@gmail.com>
2019-01-22 00:57:06 +0100

    Opened file

--- leaky.go
+++ leaky.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"github.com/xi2/xz"
+)
 
-func main()  {
-	fmt.Println("Hello World!")
+func readgz(file io.Reader) *gzip.Reader {
+	res, err := gzip.NewReader(file)
+	if err != nil {
+		panic(err)
+	}
+
+	return res
+}
+
+func readxz(file io.Reader) *xz.Reader {
+	res, err := xz.NewReader(file, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	return res
+}
+
+func main() {
+	tarfile := os.Args[1]
+
+	fmt.Println("Start indexing of " + tarfile)
+
+	f, err := os.Open(tarfile)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	switch filepath.Ext(tarfile) {
+	case ".gz":
+		readgz(f)
+	case ".xz":
+		readxz(f)
+	default:
+		fmt.Println("Extension not recognized", filepath.Ext(tarfile))
+	}
 }
